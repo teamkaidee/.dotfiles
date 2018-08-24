feeb040ecfadd9af6f6af38f4d0ccdbddb00a14a
Newcomer <newcomer@COH000232.local>
2018-08-24 16:20:16 +0700

    Skip installation of Homebrew if it's already installed.

--- install/brew.sh
+++ install/brew.sh
@@ -1,11 +1,13 @@
-echo "Installing Homebrew."
+if test ! $(which brew); then
+  echo "Installing Homebrew."
 
-if ! is-macos -o ! is-executable ruby -o ! is-executable curl -o ! is-executable git; then
-  echo "Skipped: Homebrew (missing: ruby, curl and/or git)"
-  return
+  if ! is-macos -o ! is-executable ruby -o ! is-executable curl -o ! is-executable git; then
+    echo "Skipped: Homebrew (missing: ruby, curl and/or git)"
+    return
+  fi
+
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # If installation failed try the following:
 # sudo chown -R $(whoami) /usr/local
