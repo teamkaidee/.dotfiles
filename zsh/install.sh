6c1620953bdfe1b6c67c360ffa46e805d6b95e02
Newcomer <newcomer@COH000232.local>
2018-09-04 15:36:19 +0700

    Restore ~/.zshrc.pre-oh-my-zsh if exists.

--- zsh/install.sh
+++ zsh/install.sh
@@ -8,4 +8,9 @@ fi
 echo "Installing Oh-My-ZSH! into: $ZSH"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+if [ -f ~/.zshrc.pre-oh-my-zsh ] || [ -h ~/.zshrc.pre-oh-my-zsh ]; then
+  echo "Found and restored ~/.zshrc.pre-oh-my-zsh"
+  mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
+fi
+
 chsh -s $(which zsh)
