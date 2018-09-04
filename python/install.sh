84e5999103ab57d6f7482bc520a65cf022a53a73
Newcomer <newcomer@COH000232.local>
2018-09-04 11:36:29 +0700

    Add comment on chown for python.

--- python/install.sh
+++ python/install.sh
@@ -1,5 +1,7 @@
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 
+
+# Might need to switch to use pyenv's python instead so we don't need to do this.
 sudo chown -R $USER /Library/Python/2.7
 
 python get-pip.py
