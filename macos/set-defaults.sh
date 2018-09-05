87ba81c0f9365f7b95f1d6110bd44c96b2e97172
Newcomer <newcomer@COH000232.local>
2018-09-05 18:11:18 +0700

    Show hidden files by default.

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -7,6 +7,17 @@
 #
 # Run ./set-defaults.sh and you'll be good to go.
 
+###############################################################################
+# Finder                                                                      #
+###############################################################################
+
+## Show hidden files by default
+defaults write com.apple.Finder AppleShowAllFiles -bool true
+## Show files' extension by default
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+## Stop writing DS_Store file
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
 
