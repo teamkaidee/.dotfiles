f753125fb47ebd3a43287b9f166e43e3a5647264
Newcomer <newcomer@COH000232.local>
2018-09-05 14:11:22 +0700

    Add Plugin installation for VSCode

---
+++ vscode/install.sh
@@ -0,0 +1,14 @@
+# VSCode itself is installed via Homebrew.
+# Only plugins will be installed here.
+
+# Install Standard plugins
+code --install-extension dkundel.vscode-new-file 
+code --install-extension ms-python.python
+code --install-extension donjayamanne.githistory
+code --install-extension esbenp.prettier-vscode
+
+# Install JS plugins
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eg2.vscode-npm-script
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension jpoissonnier.vscode-styled-components
