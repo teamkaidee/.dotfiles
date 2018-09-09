59286fa220f01d2a0bdbc8e5b81d1f118f532906
IT <it@ITs-MacBook-Pro.local>
2018-09-10 00:34:01 +0700

    Change jump to start/finish.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,6 +1,6 @@
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^?' backward-delete-char
+bindkey '^[[1;9D' beginning-of-line
+bindkey '^[[1;9C' end-of-line
+# bindkey '^[[3~' delete-char
+# bindkey '^?' backward-delete-char
