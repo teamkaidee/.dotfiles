224d7505da936531c82f3121502e9cb430a3733d
overskylab <teerawat.n@kaidee.com>
2019-09-03 20:57:52 +0700

    Add spaceship theme

--- fonts/install.sh
+++ fonts/install.sh
@@ -9,7 +9,7 @@ if [ ! -d "$powerline_folder" ]; then
     rm -rf "$DOTFILES_ROOT/fonts/powerline-fonts"
 fi
 
-# Installing Solarized 
+# Installing Solarized
 solarized_folder="$DOTFILES_ROOT/zsh/solarized"
 if [ ! -d "$solarized_folder" ]; then
     echo "Cloning Solarzied into $solarized_folder"
@@ -17,4 +17,15 @@ if [ ! -d "$solarized_folder" ]; then
 else
     echo "Pulling Solarzied in $DOTFILES_ROOT/zsh/solarized"
     git -C "$DOTFILES_ROOT/zsh/solarized" pull
+fi
+
+# Install spaceship prompt and theme
+spaceship_folder="$ZSH_CUSTOM/themes/spaceship-prompt"
+if [ ! -d "$spaceship_folder" ]; then
+    echo "Cloning SpaceShip into $spaceship_folder"
+    git clone https://github.com/denysdovhan/spaceship-prompt.git "$spaceship_folder"
+    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+else
+    echo "Pulling SpaceShip in $spaceship_folder"
+    git -C "$spaceship_folder" pull
 fi
