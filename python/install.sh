a2a9c8de37dca10e4b17ad67c84bee14d245256b
Victor Hazali <victorhazali@gmail.com>
2023-06-20 15:14:41 +0800

    only specify up to minor version for pyenv installations

--- python/install.sh
+++ python/install.sh
@@ -7,9 +7,9 @@ eval "$(pyenv init -)"
 # Installing python versions that our backend repo uses
 # skips installation if version already installed
 echo "installing required python versions"
-pyenv install -s  3.7.16
-pyenv install -s  3.8.16
-pyenv install -s  3.10.10
+pyenv install -s  3.7
+pyenv install -s  3.8
+pyenv install -s  3.10
 pyenv install -s  3.11
 
 echo "setting global python version to 3.11"
